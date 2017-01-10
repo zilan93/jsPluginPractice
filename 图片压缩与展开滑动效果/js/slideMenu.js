6f9e79692c6897f9738831ca75b25dc3bda43903
yangqin <zilan93@126.com>
2017-01-10 23:17:25 +0800

    0110

--- 图片压缩与展开滑动效果/js/slideMenu.js
+++ 图片压缩与展开滑动效果/js/slideMenu.js
@@ -8,68 +8,93 @@ var slide = function () {
      * d----持续时间
      * g----像素间隔
      */
-    var o,m,n,a,mw,d,g,timer,htimer;
+    var o,m,n,a,mw,d,g,offsetWid;
     return {
+        destruct:function () {
+            if(o) {
+                clearInterval(o.timer);
+                clearInterval(o.htimer);
+            }
+        },
         build:function (sm,w,t,s,x) {
             o = document.getElementById(sm);
             m = w;
             d = t;
             g = s;
             a = $(o).find("li");
-            var offsetWid = o.offsetWidth;
+            offsetWid = o.offsetWidth;
             n = offsetWid / a.length;
-            mw = (offsetWid - m)/ (a.length - 1);
+            mw = Math.floor((offsetWid - m)/ (a.length - 1));
             for(var i = 0 ; i < a.length; i++) {
+                a[i].style.width = n + "px";
                 this.timer(a[i]);
             }
             if(x) {
-                setInterval(function () {
-                    slide.destruct(a[x]);
+               o.timer = setInterval(function () {
+                    $(a[x]).find(".cover").hide();
+                    slide.slide(a[x]);
                 },d)
             }
         },
         timer:function (obj) {
             obj.onmouseover = function () {
-                clearInterval(timer);
-                clearInterval(htimer);
-                timer = setInterval(function () {
-                    slide.destruct(obj)
-                },d)
+                clearInterval(o.timer);
+                clearInterval(o.htimer);
+                o.timer = setInterval(function () {
+                    slide.slide(obj);
+                },d);
+                $(obj).find(".cover").hide();
             };
             obj.onmouseout = function () {
-                clearInterval(timer);
-                clearInterval(htimer);
-                htimer = function () {
-                    slide.destruct(obj,true)
-                }
+                clearInterval(o.timer);
+                clearInterval(o.htimer);
+                o.htimer = setInterval(function () {
+                    slide.slide(obj,true);
+                },d);
+                $(".cover").show();
+                $(obj).find(".cover").hide();
             }
         },
         slide:function (obj,state) {
-            var currWid = $(obj).width();
+            var currWid = parseInt(obj.style.width);
             if(currWid < m && !state || currWid > n && state) {
                 var widValue = 0;
+                var otherWid = 0;
+                var oi;
                 for(var i = 0; i < a.length; i++) {
                     if(a[i] != obj) {
+                        otherWid = parseInt(a[i].style.width);
                         //对目标对象外的其它对象进行操作
-                        if(currWid < m && !state) {
-                            //展开操作
-                            widValue += currWid - g;
-                            $(a[i]).css("width",currWid -g);
-                        } else if(currWid > n && state) {
-                            //收缩操作
-                            widValue += currWid
+                        //这里的if判断是针对目标对象以外的其他对象，决定的一个宽度条件
+                        if(otherWid > mw && !state) {
+                            //目标对象展开操作，其他对象收缩操作
+                            oi = Math.floor((otherWid - mw) / g);
+                            oi = oi > 0 ? oi : 1;
+                            a[i].style.width = otherWid - oi + "px";
+                        } else if(otherWid < n && state) {
+                            //目标对象收缩操作，即其他对象扩宽操作，当前宽度小于平均宽度，就进行操作
+                            oi = Math.floor((n - otherWid) / g);
+                            oi = oi > 0 ? oi : 1;
+                            a[i].style.width = otherWid + oi + "px";
                         }
-
+                        if(state) {
+                            widValue += (otherWid + oi);
+                        } else {
+                            widValue += (otherWid - oi);
+                        }
                     }
                 }
+                obj.style.width = offsetWid - widValue + "px";
             } else {
-                clearInterval(timer);
-                clearInterval(htimer);
-            }
-        },
-        destruct:function () {
+                if(o.timer) {
+                    clearInterval(o.timer);
+                }
+                if(o.htimer) {
+                    clearInterval(o.htimer);
+                }
 
+            }
         }
     }
 }();
-slide.build("sm",452,10,2,1);
+slide.build("sm",450,10,10,1);
