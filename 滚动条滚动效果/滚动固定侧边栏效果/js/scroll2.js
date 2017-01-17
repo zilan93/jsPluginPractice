de9a626d8e02a9d18aa28e00e4c0c60a8873bac9
yangqin <zilan93@126.com>
2017-01-17 16:16:09 +0800

    0117

--- 滚动条滚动效果/滚动固定侧边栏效果/js/scroll2.js
+++ 滚动条滚动效果/滚动固定侧边栏效果/js/scroll2.js
@@ -1,14 +1,12 @@
-function scrollAside() {
-     //获取相应变量
     var winHei = $(window).height();
     var a = $(".aside").height();
     var c = $(".content").height();
     var offsetTop = $(".content").offset().top;
-    var $header = $(".header");
-    //滚动处理事件
-    var operation = function(state) {
-        if(state) {
-            //向下滚动
+function scrollAside() {
+    //即当右侧高度小于左侧时，才会有滚动处理
+    if(a < c) {
+        //滚动处理事件
+        ////向下滚动
             var e = document.body.scrollTop;
             var d = a - (e - 80);
             //判断侧边是否滚到底部
@@ -27,13 +25,12 @@ function scrollAside() {
                         "bottom":"auto"
                     })
                 }
-            }
-        } else {
-            //向上滚动
-            e = document.body.scrollTop - 80;
-            d = e - (c - a);
+            } else {
+                //向上滚动
+            m = document.body.scrollTop - 80;
+            n = m - (c - a);
             //侧边滚动的高度小于等于0
-            if(d <= 0) {
+            if(n <= 0) {
                 $(".aside").css({
                     "position":"fixed",
                     "top":"0",
@@ -41,36 +38,19 @@ function scrollAside() {
                     "bottom":"auto"
                 });
                 //主体内容是否滚出头部
-                if(e <= 0) {
+                if(m <= 0) {
                     $(".aside").css({
                         "position":"absolute",
                         "top":offsetTop
                     })
                 }
             };
-
-        }
-    };
-    //即当右侧高度小于左侧时，才会有滚动处理
-    if(a < c) {
-        //判断滚动方向，绑定滚动处理事件
-        var beforeScrollTop = document.body.scrollTop;
-        $(window).scroll(function () {
-            var afterScrollTop = document.body.scrollTop,
-                delta = afterScrollTop - beforeScrollTop,
-                state = true;
-            if(delta == 0) return false;
-            if(delta > 0) {
-                //向下滚动
-                state = true;
-                operation(state);
-            } else if(delta < 0) {
-                //向上滚动
-                state = false;
-                operation(state);
-            }
-            beforeScrollTop = afterScrollTop;
-        })
+        }     
     }
 }
-scrollAside();
+$(window).scroll(function () {
+    scrollAside();
+});
+$(function() {
+    scrollAside();
+})
